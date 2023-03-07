ac71f23d1be05ea1bb08a359227f02684e264ce6
YuriySamusew <6177385@gmail.com>
2023-03-08 00:35:53 +0200

    fetchOllContacts ok

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,15 +3,29 @@ import ContactList from 'components/ContactsList/ContactsList';
 import Filter from 'components/Filter/Filter';
 
 import { Title, Wrapper } from 'components/App.styled';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectError, selectIsLoading } from 'redux/selectors';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/operations';
 
 const App = () => {
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <Wrapper>
       <Title title="Phonebook">Phonebook</Title>
       <ContactForm />
       <Title title="Contacts">Contacts</Title>
       <Filter />
-      <ContactList />
+      {isLoading && !error && <p>...Loading</p>}
+      {error && <p>{error}</p>}
+      {!isLoading && <ContactList />}
     </Wrapper>
   );
 };
