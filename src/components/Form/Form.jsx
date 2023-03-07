0089c034f283522a5a3999cea4b20f915371a537
YuriySamusew <6177385@gmail.com>
2023-03-07 12:42:55 +0200

    fixed checks contact form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -55,8 +55,12 @@ const ContactForm = () => {
         <Input
           type="text"
           name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          required
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          value={name}
           onChange={handleChange}
+          placeholder="Name"
         />
       </Label>
       <Label htmlFor="number">
@@ -64,8 +68,12 @@ const ContactForm = () => {
         <Input
           type="tel"
           name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          required
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          value={number}
           onChange={handleChange}
+          placeholder="+312345678"
         />
       </Label>
 
